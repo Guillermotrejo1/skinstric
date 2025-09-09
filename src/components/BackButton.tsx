a8a5426d369593b91578af48d9d0e6efcf272249
Guillermo Trejo <maynardtrejo@yahoo.com>
2025-09-09 17:33:33 -0500

    added the proceed button on small screens and larger screens

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -16,15 +16,19 @@ const BackButton = () => {
   return (
     <div
       onClick={handleBackClick}
-      className="absolute bottom-38.5 md:bottom-8 w-full flex justify-between md:px-9 px-13"
+      className="absolute left-2 bottom-38.5 md:bottom-8 flex justify-between md:px-9 px-13"
     >
       <Link href="/" aria-label="Back" className="inset-0">
-        <div>
-          <div className="relative w-12 h-12 flex items-center justify-center border border-[#1A1B1C] rotate-45 scale-[1] sm:hidden">
-            <span className="rotate-[-45deg] text-sm font-semibold sm:hidden">
-              Back
-            </span>
+        <div className="relative w-12 h-12 flex items-center justify-center border border-[#1A1B1C] rotate-45 scale-[1] md:hidden">
+          <span className="rotate-[-45deg] text-sm font-semibold md:hidden">
+            Back
+          </span>
+        </div>
+        <div className="hidden md:flex lg:flex">
+          <div className="relative w-12 h-12 flex items-center justify-center border border-[#1A1B1C] rotate-45 scale-[1]">
+            <span className="rotate-[-225deg] text-sm font-semibold">▶</span>
           </div>
+          <span className="ml-8 text-sm mt-3 font-semibold">BACK</span>
         </div>
       </Link>
     </div>
---
+++ src/components/ProceedButton.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import React from "react";
+
+const ProceedButton = () => {
+  return (
+    <div
+      className="absolute right-10 md:right-30 w-full bottom-36 md:bottom-8 flex justify-between md:px-9 px-13"
+    >
+      <Link href="/results" aria-label="Proceed" className="inset-0">
+        <div className="relative w-12 h-12 flex items-center justify-center border border-[#1A1B1C] rotate-45 scale-[1] md:hidden">
+          <span className="rotate-[-45deg] text-xs font-semibold md:hidden">
+            PROCEED
+          </span>
+        </div>
+        <div className="hidden md:flex lg:flex items-center">
+          <span className="mr-5 text-sm mt-3 font-semibold">PROCEED</span>
+          <div className="relative w-12 h-12 flex items-center justify-center border border-[#1A1B1C] rotate-45 scale-[1]">
+            <span className="rotate-[-45deg] text-sm font-semibold">▶</span>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default ProceedButton;
