c1d8ca53941d906d4965b7b260401105404f6e39
Guillermo Trejo <maynardtrejo@yahoo.com>
2025-09-09 14:58:03 -0500

    made changes to the loading state

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -14,6 +14,7 @@ const Test = () => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [answers, setAnswers] = useState({ name: "", location: "" });
   const [error, setError] = useState("");
+  const [submissionStatus, setSubmissionStatus] = useState("");
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -36,6 +37,7 @@ const Test = () => {
     setError("");
 
     if (currentQuestionIndex === questions.length - 1) {
+      setSubmissionStatus("processing");
       try {
         const response = await fetch(
           "https://us-central1-api-skinstric-ai.cloudfunctions.net/skinstricPhaseOne",
@@ -59,12 +61,16 @@ const Test = () => {
 
         const data = await response.json();
         console.log("API Response:", data);
+        setTimeout(() => {
+          setSubmissionStatus("success");
+        }, 3000);
 
         setAnswers({ name: "", location: "" });
         setCurrentQuestionIndex(0);
       } catch (error) {
         console.error("There was a problem with the fetch operation:", error);
         setError("Failed to fetch data from the API.");
+        setSubmissionStatus("");
       }
     } else {
       setCurrentQuestionIndex(currentQuestionIndex + 1);
@@ -79,41 +85,67 @@ const Test = () => {
           <h4 className="ml-6 text-xs font-semibold">TO START ANALYSIS</h4>
         </div>
         <div className="relative flex flex-col items-center justify-center mb-40 w-full h-full">
-          <p className="text-sm text-gray-400 tracking-wider uppercase mb-1">
-            {" "}
-            Click To Type{" "}
-          </p>
-          <form className="relative z-10" onSubmit={handleSubmit}>
-            <div className="flex flex-col items-center">
-              {error && <p className="text-red-500 text-sm mb-2">{error}</p>}
+          {submissionStatus === "processing" ? (
+            <div className="flex flex-col">
+              <p className="text-sm text-gray-400 tracking-wider uppercase mb-1">
+                Processing submission
+                <div>
+                  <span className="inline-block w-1"></span>
+                  <span className="text-7xl dot"></span>
+                  <span className="text-7xl dot"></span>
+                  <span className="text-7xl dot"></span>
+                </div>
+              </p>
             </div>
-            <input
-              type="text"
-              placeholder={questions[currentQuestionIndex].placeholder}
-              autoComplete="off"
-              tabIndex={0}
-              name="answer"
-              value={
-                currentQuestionIndex === 0 ? answers.name : answers.location
-              }
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                setAnswers({
-                  ...answers,
-                  [currentQuestionIndex === 0 ? "name" : "location"]:
-                    e.target.value,
-                });
-                setError("");
-              }}
-              className="text-5xl sm:text-6xl font-normal text-center bg-transparent border-b border-black focus:outline-none appearance-none w-[372px] sm:w-[432px] pt-1 tracking-[-0.07em] leading-[64px] text-[#1A1B1C] z-20 relative"
-            />
-            <button type="submit" className="sr-only">
-              {" "}
-              Submit{" "}
-            </button>
-            <DiamondLarge />
-            <DiamondMedium />
-            <DiamondSmall />
-          </form>
+          ) : submissionStatus === "success" ? (
+            <div>
+              <p className="text-xl text-[#1a1b1c] tracking-wider uppercase mb-4">
+                Thank you!
+              </p>
+              <p className="text-gray-600 text-lg">Proceed for the next step</p>
+            </div>
+          ) : (
+            <p className="text-sm text-gray-400 tracking-wider uppercase mb-1">
+              Click To Type
+            </p>
+          )}
+
+          <DiamondLarge />
+          <DiamondMedium />
+          <DiamondSmall />
+
+          {submissionStatus !== "processing" &&
+            submissionStatus !== "success" && (
+              <form className="relative z-10" onSubmit={handleSubmit}>
+                <div className="flex flex-col items-center">
+                  {error && (
+                    <p className="text-red-500 text-sm mb-2">{error}</p>
+                  )}
+                </div>
+                <input
+                  type="text"
+                  placeholder={questions[currentQuestionIndex].placeholder}
+                  autoComplete="off"
+                  tabIndex={0}
+                  name="answer"
+                  value={
+                    currentQuestionIndex === 0 ? answers.name : answers.location
+                  }
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    setAnswers({
+                      ...answers,
+                      [currentQuestionIndex === 0 ? "name" : "location"]:
+                        e.target.value,
+                    });
+                    setError("");
+                  }}
+                  className="text-5xl sm:text-6xl font-normal text-center bg-transparent border-b border-black focus:outline-none appearance-none w-[372px] sm:w-[432px] pt-1 tracking-[-0.07em] leading-[64px] text-[#1A1B1C] z-20 relative"
+                />
+                <button type="submit" className="sr-only">
+                  Submit
+                </button>
+              </form>
+            )}
         </div>
         <BackButton />
       </div>
