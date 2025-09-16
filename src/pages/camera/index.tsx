b33afa8afaf480ad5ca2fde2978038cdc5b955d4
Guillermo Trejo <maynardtrejo@yahoo.com>
2025-09-16 15:44:27 -0500

    removed some future code changes

--- src/pages/camera/index.tsx
+++ src/pages/camera/index.tsx
@@ -3,13 +3,13 @@ import { useRouter } from "next/router";
 import React, { useEffect } from "react";
 
 const Index = () => {
- const router = useRouter();
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      router.push('/camera/capture');
-    }, 3000);
-    return () => clearTimeout(timer);
-  }, []);
+//  const router = useRouter();
+//   useEffect(() => {
+//     const timer = setTimeout(() => {
+//       router.push('/camera/capture');
+//     }, 3000);
+//     return () => clearTimeout(timer);
+//   }, []);
 
   return (
     <>
@@ -23,9 +23,9 @@ const Index = () => {
         />
         <p className="text-lg mt-2 md:opacity-50">SETTING UP CAMERA...</p>
         <div>
-          <div className="absolute top-[calc(50%-125px)] md:top-[calc(50%-50px)] left-1/2 -translate-x-[50%] -translate-y-1/2 w-[260px] h-[260px] md:w-[360px] md:h-[360px] animate-spin-faster rotate-190 border-3 border-[#A0A4AB] border-dotted opacity-20"></div>
-          <div className="absolute top-[calc(50%-125px)] md:top-[calc(50%-50px)] left-1/2 -translate-x-[50%] -translate-y-1/2 w-[240px] h-[240px] md:w-[340px] md:h-[340px] animate-spin-faster rotate-185 border-3 border-[#A0A4AB] border-dotted opacity-30"></div>
-          <div className="absolute top-[calc(50%-125px)] md:top-[calc(50%-50px)] left-1/2 -translate-x-[50%] -translate-y-1/2 w-[220px] h-[220px] md:w-[320px] md:h-[320px] animate-spin-fast border-3 border-[#A0A4AB] border-dotted opacity-40"></div>
+          <div className="absolute max-[400px]:top-[calc(50%-90px)] top-[calc(50%-125px)] md:top-[calc(50%-50px)] left-1/2 -translate-x-[50%] -translate-y-1/2 w-[260px] h-[260px] md:w-[360px] md:h-[360px] animate-spin-faster rotate-190 border-3 border-[#A0A4AB] border-dotted opacity-20"></div>
+          <div className="absolute max-[400px]:top-[calc(50%-90px)] top-[calc(50%-125px)] md:top-[calc(50%-50px)] left-1/2 -translate-x-[50%] -translate-y-1/2 w-[240px] h-[240px] md:w-[340px] md:h-[340px] animate-spin-faster rotate-185 border-3 border-[#A0A4AB] border-dotted opacity-30"></div>
+          <div className="absolute max-[400px]:top-[calc(50%-90px)] top-[calc(50%-125px)] md:top-[calc(50%-50px)] left-1/2 -translate-x-[50%] -translate-y-1/2 w-[220px] h-[220px] md:w-[320px] md:h-[320px] animate-spin-fast border-3 border-[#A0A4AB] border-dotted opacity-40"></div>
         </div>
       </div>
       <div className="w-full flex flex-col items-center justify-center mb-8 max-w-[600px] md:max-w-full">
